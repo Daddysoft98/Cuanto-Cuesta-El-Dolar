28e89fd788296cc9f73e4fe386ebff93cd3fca7e
Daddysoft <designhub0398@gmail.com>
2024-10-10 01:25:56 -0500

    Añadido logo en la cabecera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,28 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component'; 
-import { MainContentComponent } from './components/main-content/main-content.component';
-import { ExchangeFormComponent } from './components/exchange-form/exchange-form.component'; 
+import { MainContentComponent } from './components/main-content/main-content.component'; 
 import { CurrencyExchangesComponent } from './components/currency-exchanges/currency-exchanges.component';
-import { FooterComponent } from './components/footer/footer.component'; // Importar FooterComponent
+import { ExchangeFormComponent } from './components/exchange-form/exchange-form.component';
+import { FooterComponent } from './components/footer/footer.component'; 
+import { CommonModule } from '@angular/common'; // Necesario para NgFor, NgIf, etc.
 
 @Component({
   selector: 'app-root',
   template: `
     <app-header></app-header>  
-    <app-main-content></app-main-content>  
-    <app-exchange-form></app-exchange-form> 
+    <app-main-content></app-main-content>   
     <app-currency-exchanges></app-currency-exchanges>
-    <app-footer></app-footer> <!-- Asegúrate de incluir el footer aquí -->
+    <app-exchange-form></app-exchange-form>
+    <app-footer></app-footer> 
   `,
   standalone: true,
   imports: [
     HeaderComponent,
     MainContentComponent,
-    ExchangeFormComponent,
     CurrencyExchangesComponent,
-    FooterComponent // Asegúrate de incluir el FooterComponent
+    ExchangeFormComponent,
+    FooterComponent,
+    CommonModule 
   ]
 })
 export class AppComponent {}
